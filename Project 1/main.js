6d40ac74ac6c0b70b013d0716e99fcb17754f437
Gail Leanne Loking <gailloks123@gmail.com>
2025-03-12 20:04:39 +0800

    Delete Project 1 directory

--- Project 1/main.js
+++
@@ -1,7 +0,0 @@
-window.onresize = screen;
-window.onload = screen;
-
-function screen() {
-    Width = window.innerWidth;
-    document.getElementById("size").innerHTML = "Width: " + Width + " px"
-}
